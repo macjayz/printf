fb3b671c9b550b4ff08260b98983c4ef713e980c
macjayz <machenjosephjohn@gmail.com>
2023-01-23 08:17:07 -0800

    New code added

--- _printf.c
+++ _printf.c
@@ -13,28 +13,22 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
+	int i, count, value = 0;
 	va_list args;
 	int (*f)(va_list);
 
-	va_start(args, format);	
-	/*Prevent parsing a null pointer*/
+	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	
-	/*Print each character of string*/
 	while (format[i])
-	{	
+	{
 		if (format[i] != '%')
 		{
-			value = write(1,&format[i],1);
+			value = write(1, &format[i], 1);
 			count = count + value;
 			i++;
 			continue;
 		}
-
 		if (format[i] == '%')
 		{
 			f = check_specifier(&format[i + 1]);
@@ -45,24 +39,18 @@ int _printf(const char *format, ...)
 				i = i + 2;
 				continue;
 			}
-
 			if (format[i + 1] == '\0')
-			{
 				break;
-			}
-
 			if (format[i + 1] != '\0')
 			{
 				value = write(1, &format[i + 1], 1);
 				count = count + value;
-                        	i = i + 2;
-                        	continue;
+			i = i + 2;
+			continue;
 			}
-
 
 		}
 	}
-
 	return (count);
 }
 
